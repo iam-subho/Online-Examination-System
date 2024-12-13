16afaeca0503a3a4e0637794270d266d37b96866
iam-subho <55249273+iam-subho@users.noreply.github.com>
2024-12-13 11:56:49 +0530

    constants config file added

---
+++ config/constants.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'errorEvent' => 'showErrorMessage',
+    'successEvent' => 'showErrorMessage',
+
+    'no_permission_page_text' => 'You dont have permission to access this page!',
+    'no_permission_action_text' => 'You dont have permission for this action!',
+
+    'unauthorization_text' => 'Unauthorized access!',
+
+];
